00d83d09621e2f39b44e3a20733a4bf8ee4e4854
YuriOC <yuriuinke@gmail.com>
2025-12-16 16:23:04 -0300

    Jackson dependency and getJson class added

--- src/main/java/br/com/alura/vehicles_price_search/models/VehiclesPriceSearchApplication.java
+++ src/main/java/br/com/alura/vehicles_price_search/models/VehiclesPriceSearchApplication.java
@@ -1,14 +1,27 @@
 package br.com.alura.vehicles_price_search.models;
 
+import br.com.alura.vehicles_price_search.services.GetJsonFromURL;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.json.JsonParser;
 
 @SpringBootApplication
-public class VehiclesPriceSearchApplication {
+public class VehiclesPriceSearchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehiclesPriceSearchApplication.class, args);
-        System.out.println("Hello World");
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        GetJsonFromURL JsonFromUrl = new GetJsonFromURL();
+        System.out.println("hello world");
+
+        String json = JsonFromUrl.getJsonFromURL("https://parallelum.com.br/fipe/api/v1/carros/marcas");
+        System.out.println(json);
+
+
+    }
 }
