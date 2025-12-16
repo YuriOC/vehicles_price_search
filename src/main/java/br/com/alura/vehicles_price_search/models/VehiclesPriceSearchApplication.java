bb51e91a8d4e8e9d58f6b06d15efb2117a1a9774
YuriOC <yuriuinke@gmail.com>
2025-12-16 15:32:49 -0300

    First commit

---
+++ src/main/java/br/com/alura/vehicles_price_search/models/VehiclesPriceSearchApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.vehicles_price_search.models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesPriceSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesPriceSearchApplication.class, args);
+        System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/br/com/alura/vehicles_price_search/VehiclesPriceSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.vehicles_price_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesPriceSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
